3d21de7ad779756c5fc32bea63b510e0230477a5
Nicholas Jackson <smartguy1196@gmail.com>
2020-12-30 19:22:37 -0800

    install.js exit code 1 fix attempt

--- install.js
+++ install.js
@@ -1,5 +1,12 @@
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
 console.log( "This script is run at the end of the nodejs installation" )
 console.log( "Bye!" )
-
-fs = require('fs');
-fs.writeFile( ".\\test.txt", "hello there" )
